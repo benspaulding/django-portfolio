f0d7265994c7f5b160395d49b8bd0321eefd7a93
Ben Spaulding <ben@benspaulding.us>
2012-11-04 15:24:33 -0600

    Keep package from erroring before Django is installed.

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,5 +1,7 @@
-from django.utils.translation import ugettext_lazy as _
-
-
-# Mark the app_label for translation.
-_(u'portfolio')
+# Mark the app_label for translation. Use try/except to avoid ``ImportError``
+# when setup imports version before Django is installed.
+try:
+    from django.utils.translation import ugettext_lazy as _
+    _(u'portfolio')
+except ImportError:
+    pass
